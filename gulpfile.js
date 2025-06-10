e88f6b7c781f875cf38630ea516f38fc47adb3d8
JoannEmawodia <emawodia@gmail.com>
2025-06-10 08:42:35 -0300

    minificando imagens

--- gulpfile.js
+++ gulpfile.js
@@ -2,4 +2,17 @@ const gulp = require ('gulp');
 const sass = require ('gulp-sass')(require ('sass'));
 const imagemin = require ('gulp-imagemin');
 
-exports.default = gulp;
+function styles (){
+    return gulp.src('./src/styles/*.scss')
+        .pipe(sass ({outputStyle: 'compressed'}))
+        .pipe(gulp.dest('./dist/.css'));
+}
+
+function images (){
+    return gulp.src('./src/imagens/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/imagens'));
+}
+
+
+exports.default = gulp.parallel(styles, images);
